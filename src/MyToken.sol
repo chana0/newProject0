0fba76a086f6213673e3b400789d39ca59dd8e13
chana0 <chana01021@gmail.com>
2024-04-11 16:55:48 +0300

    update0

---
+++ src/MyToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+    function mint(address add,uint amount)public
+    {
+        _mint(add,amount);
+    }
+    myToken.transform(msg.sender,address(this))
+}
